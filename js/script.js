70d6c86db536113afd07abedf91029540274fbd1
njelevatedmedia <njelevatedmedia@gmail.com>
2026-07-14 16:46:39 -0400

    Saved Script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
+
+menuToggle.addEventListener("click", () => {
+
+    mainNav.classList.toggle("active");
+
+});
